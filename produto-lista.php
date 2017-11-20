44aad52f9611cfc10d0bc8bf506b0dcf05ce2ce9
Victor Buoro da Silva <v.buoro@coblue.com.br>
2017-11-20 17:31:03 -0200

    criado rascunho da pagina Sobre, trazendo lista de produtos

--- produto-lista.php
+++ produto-lista.php
@@ -3,7 +3,7 @@
 <?php
   $resultado = mysqli_query($conexao, "select * from produtos");
   while ($produto = mysqli_fetch_assoc($resultado)) {
-    echo $produto['nome']."<br />"
+    echo $produto['nome']."<br />";
   }
 ?>
 <?php include("rodape.php");?>
---
+++ sobre.php
@@ -0,0 +1,3 @@
+<?php require ('cabecalho.php');?>
+<h1>Página sobre a loja</h>
+<?php require('rodape.php');?>
