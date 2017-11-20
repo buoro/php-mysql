3a61364271a1af19edd9fb1839b13a047d267cff
Victor Buoro da Silva <v.buoro@coblue.com.br>
2017-11-20 19:31:25 -0200

    ao excluir produto, redireciona para a própria página ao inves de exibir a mensagem

--- remove-produto.php
+++ remove-produto.php
@@ -5,7 +5,5 @@ include('banco-produto.php');
 
 $id=$_GET['id'];
 removeProduto($conexao, $id);
-?>
-
-<p class="text-success">Produto <?=$id?> removido com sucesso</p>
-<?php include('rodape.php');?>
+header("Location: produto-lista.php");
+die();
